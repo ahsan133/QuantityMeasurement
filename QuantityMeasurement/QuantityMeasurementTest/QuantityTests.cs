17b99ea096869cadfbbafd06fe736a11da3e24e3
ahsan133 <mahsanullah512@gmail.com>
2020-09-18 13:04:19 +0530

    [MD Ahsanullah] Add: test case 1.3 reference check is added

--- QuantityMeasurement/QuantityMeasurementTest/QuantityTests.cs
+++ QuantityMeasurement/QuantityMeasurementTest/QuantityTests.cs
@@ -15,11 +15,20 @@ namespace QuantityMeasurementTest
         }
 
         [TestMethod]
-        public void GivenZeroFeetToNull_ThenCompareQuantity_ShouldReturnNotEquals()
+        public void GivenZeroFeetToNull_WhenComparedQuantity_ThenShouldReturnNotEquals()
         {
             LengthCompare lengthOne = new LengthCompare(LengthCompare.Unit.FEET, 0.0);
             LengthCompare lengthTwo = null;
             Assert.AreNotEqual(lengthOne, lengthTwo);
+        }
+
+        [TestMethod]
+        public void GivenTwoObjects_WhenCheckReference_ThenShouldReturnFalse()
+        {
+            LengthCompare lengthOne = new LengthCompare(LengthCompare.Unit.FEET, 0.0);
+            LengthCompare lengthTwo = new LengthCompare(LengthCompare.Unit.FEET, 1.0);
+            bool toReturn = System.Object.ReferenceEquals(lengthOne, lengthTwo);
+            Assert.IsFalse(toReturn);
         }
     }
 }
